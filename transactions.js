8404bf6155c02712a94308888e91b891893efce3
Gabrielle Guarino <gguarino@umich.edu>
2016-05-13 14:58:13 -0400

    Delete transactions.js

--- transactions.js
+++
@@ -1,9 +0,0 @@
-app.factory('transactions', ['$http', function($http) { 
-  return $http.get('transactions.json') 
-	.success(function(data) { 
-	  return data; 
-	}) 
-	.error(function(err) { 
-	  return err; 
-	}); 
-}]);
