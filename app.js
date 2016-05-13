79aff133748d2cfd13c1bc7c65a4d6438f55ee21
Gabrielle Guarino <gguarino@umich.edu>
2016-05-13 15:14:03 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 var app = angular.module('search', []);
 
 app.controller('controller', ['$scope', '$http', function($scope, $http) {
-	alert("here");
 	$http.get('transactions.json')
 		.success(function(data){
 			$scope.results1 = data;
-			alert("!");
 		});
 	$http.get('funds.json')
 		.success(function(data){
 			$scope.results2 = data;
-			alert("!!!!");
 		});
 }]);
