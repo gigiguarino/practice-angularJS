c0278dfbbcdab97608daca8e1697e03cfd12c87c
Gabrielle Guarino <gguarino@umich.edu>
2016-05-13 15:01:11 -0400

    Add files via upload

--- controller.js
+++ controller.js
@@ -3,7 +3,7 @@ app.controller('controller', function($scope, $http) {
 		.success(function(data)){
 			$scope.results1 = data;
 			alert("!");
-		})
+		});
 	$http.get('funds.json')
 		.success(function(data){
 			$scope.results2 = data;
