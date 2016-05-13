a85e666bf44f58632c1c8a5c970244063eb5a8c6
Gabrielle Guarino <gguarino@umich.edu>
2016-05-13 14:17:14 -0400

    Add files via upload

--- search.js
+++ search.js
@@ -1,8 +1,12 @@
 $('#search-form').submit(function(){
 	if ($('#funds-check').checked){
 		$scope.show-funds = true;
+	} else {
+		$scope.show-funds = false;
 	}
 	if ($('#transactions-check').checked){
 		$scope.show-transactions = true;
+	} else {
+		$scope.show-transactions = false;
 	}
 });
