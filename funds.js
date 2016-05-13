3baa1927be6b87c7bed9a0de3a5527a3716f4d58
Gabrielle Guarino <gguarino@umich.edu>
2016-05-13 14:58:18 -0400

    Delete funds.js

--- funds.js
+++
@@ -1,9 +0,0 @@
-app.factory('funds', ['$http', function($http) { 
-  return $http.get('funds.json') 
-	.success(function(data) { 
-	  return data; 
-	}) 
-	.error(function(err) { 
-	  return err; 
-	}); 
-}]);
